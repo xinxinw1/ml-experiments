9a8ae584f692063c7f1cacc0fd144f2711b86d9f
Xin-Xin Wang - thevenin <xin-xinw@musiclifephilosophy.com>
2017-11-20 02:28:34 -0500

    set up ordering to pass in init states next

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,6 +1,7 @@
 import pytest
 
 import tools
+import numpy as np
 
 def test_group():
     assert list(tools.group([1, 2, 3], 2)) == [[1, 2], [3]]
@@ -15,12 +16,32 @@ def test_split_max_len():
 def test_pad_to_same_len():
     assert list(tools.pad_to_same_len([[1, 2], [1], [1, 2, 3]], 0)) == [[1, 2, 0], [1, 0, 0], [1, 2, 3]]
 
+def test_single_make_batches():
+    batches = list(tools.single_make_batches([1, 2, 3, 4, 5], 2, 2, 0))
+    expected = [
+        [[1, 2], [4, 5]],
+        [[3], [0]]
+    ]
+    for batch, expect in zip(batches, expected):
+        assert (batch == expect).all()
+
 def test_make_batches():
-    assert (list(tools.make_batches([[1, 2, 3, 4], [1]], 2, 3, 0, 0)) == [
-        ([[0, 1, 2], [3, 4, 0]], [[1, 2, 3], [4, 0, 0]]),
-        ([[0, 1]], [[1, 0]])
-    ])
+    batches = list(tools.make_batches([[1, 2, 3, 4], [1]], 2, 3, 0, 0))
+    print(batches)
+    expected = [
+        ([[0, 1, 2], [4, 0, 1]], [[1, 2, 3], [0, 1, 0]]),
+        ([[3], [0]], [[4], [0]])
+    ] 
+    for (inputs_batch, labels_batch), (expected_inputs, expected_labels) in zip(batches, expected):
+        print(inputs_batch, labels_batch, expected_inputs, expected_labels)
+        assert (inputs_batch == expected_inputs).all()
+        assert (labels_batch == expected_labels).all()
+
     batches = tools.make_batches([[1, 2, 3, 4]], batch_size=1, max_single_len=None, token_item=0, pad_item=0)
-    assert next(batches) == ([[0, 1, 2, 3, 4]], [[1, 2, 3, 4, 0]])
+    expected_inputs = [[0, 1, 2, 3, 4]]
+    expected_labels = [[1, 2, 3, 4, 0]]
+    inputs_batch, labels_batch = next(batches)
+    assert (inputs_batch == expected_inputs).all()
+    assert (labels_batch == expected_labels).all()
     with pytest.raises(StopIteration):
         next(batches)
--- tools.py
+++ tools.py
@@ -1,4 +1,6 @@
 import shutil
+import math
+import numpy as np
 
 def group(lst, n):
     return (lst[i:i+n] for i in range(0, len(lst), n))
@@ -17,19 +19,30 @@ def pad_to_same_len(lst_of_lsts, pad_item):
     return list(map(lambda lst: pad_to_len(lst, max_len, pad_item), lst_of_lsts))
 
 def make_batches(inputs, batch_size, max_single_len, token_item, pad_item):
-    inputs_batches = map(lambda input: [token_item] + input, inputs)
-    labels_batches = map(lambda input: input + [token_item], inputs)
-    if max_single_len is not None:
-        inputs_batches = split_max_len(inputs_batches, max_single_len)
-        labels_batches = split_max_len(labels_batches, max_single_len)
-    inputs_batches = group(list(inputs_batches), batch_size)
-    labels_batches = group(list(labels_batches), batch_size)
-    inputs_batches = map(lambda batch: pad_to_same_len(batch, pad_item), inputs_batches)
-    labels_batches = map(lambda batch: pad_to_same_len(batch, pad_item), labels_batches)
+    inputs_batches = flatmap(lambda input: [token_item] + input, inputs)
+    labels_batches = flatmap(lambda input: input + [token_item], inputs)
+    inputs_batches = single_make_batches(list(inputs_batches), batch_size, max_single_len, pad_item)
+    labels_batches = single_make_batches(list(labels_batches), batch_size, max_single_len, pad_item)
 
     return zip(inputs_batches, labels_batches)
 
+def get_len_to_make_divisible(lst, num):
+    return math.ceil(len(lst) / num) * num
 
+def single_make_batches(lst, batch_size, max_single_len, pad_item):
+    """
+    lst: list of numbers
+    """
+    # goal_len is smallest number that is >= len(lst) and is divisible by batch_size
+    goal_len = get_len_to_make_divisible(lst, batch_size)
+    padded_lst = pad_to_len(lst, goal_len, pad_item)
+    long_batch = np.array(padded_lst).reshape(batch_size, -1)
+
+    if max_single_len is not None:
+        for i in range(0, long_batch.shape[1], max_single_len):
+            yield long_batch[:, i:i+max_single_len]
+    else:
+        yield long_batch
 
 def rmdir_if_exists(path):
     try:
