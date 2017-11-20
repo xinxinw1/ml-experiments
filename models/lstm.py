b09315737d1e055bcdb2cceb876f87c947551055
Xin-Xin Wang - thevenin <xin-xinw@musiclifephilosophy.com>
2017-11-20 03:09:35 -0500

    working training with init state

--- models/lstm.py
+++ models/lstm.py
@@ -47,11 +47,30 @@ class LSTMModelBase(object):
             # inputs_one_hot is a list of batches where each batch is a list of one hot encoded lists
             # inputs_one_hot is a tensor with dim batch_size x (timesteps+1) x (alphabet_size+1)
 
-            state_sizes = [2*self.effective_alphabet_size] * 1
-            lstm_cells = list(map(tf.contrib.rnn.BasicLSTMCell, state_sizes))
+            def make_init_state(i, state_size):
+                return tf.placeholder(tf.float32, [2, None, state_size], name='init_state_' + str(i))
+
+            def make_lstm_state_tuple(init_state):
+                c, h = tf.unstack(init_state)
+                return tf.contrib.rnn.LSTMStateTuple(c, h)
+
+            self.state_sizes = [2*self.effective_alphabet_size] * 1
+            self.init_states = tuple(make_init_state(i, state_size) for i, state_size in enumerate(self.state_sizes))
+            rnn_init_state = tuple(make_lstm_state_tuple(init_state) for init_state in self.init_states)
+
+            lstm_cells = list(map(tf.contrib.rnn.BasicLSTMCell, self.state_sizes))
             lstm = tf.contrib.rnn.MultiRNNCell(lstm_cells)
-            rnn_output, state = tf.nn.dynamic_rnn(lstm, self.inputs_one_hot, dtype=tf.float32)
+            rnn_output, rnn_states = tf.nn.dynamic_rnn(lstm, self.inputs_one_hot, initial_state=rnn_init_state, dtype=tf.float32)
             # rnn_outputs is a tensor with dim batch_size x (timesteps+1) x (alphabet_size+1)
+            # state is a list of tensors with dim batch_size x state_size
+
+            def make_output_state(i, state_tuple):
+                c = state_tuple.c
+                h = state_tuple.h
+                return tf.identity(tf.stack([c, h]), name='output_state_' + str(i))
+
+            self.output_states = tuple(make_output_state(i, state) for i, state in enumerate(rnn_states))
+
             logits = tf.contrib.layers.fully_connected(rnn_output, self.effective_alphabet_size, activation_fn=None)
             self.logits = tf.identity(logits, name='logits')
 
@@ -134,13 +153,24 @@ class LSTMModelBase(object):
 
         logging.info('Saved model to file %s' % file_path)
 
-    def run_batch(self, tensors, batch):
+    def run_batch_with_state(self, tensors, batch, curr_states):
         """
         batch: tuple (inputs_batch, labels_batch)
+        Returns:
+            A list of outputs [new_states] + tensor_outputs
+        """
+        return self.run_batch(tensors + [self.output_states], batch, curr_states)
+
+    def run_batch(self, tensors, batch, curr_states):
+        """
+        batch: tuple (inputs_batch, labels_batch)
+        Returns:
+            A list of outputs tensor_outputs
         """
         inputs, labels = batch
         # print(inputs, labels)
-        return self.sess.run(tensors, feed_dict={self.inputs: inputs, self.labels: labels})
+        feed_dict = {self.inputs: inputs, self.labels: labels, self.init_states: curr_states}
+        return self.sess.run(tensors, feed_dict=feed_dict)
 
     def run_single(self, tensors, lst):
         batches = tools.make_batches([lst], batch_size=1, max_single_len=None,
@@ -155,16 +185,26 @@ class LSTMModelBase(object):
         summaries_dir = save_dir
         logging.info('Using summaries dir %s' % summaries_dir)
         summary_writer = tf.summary.FileWriter(summaries_dir)
-        batches = tools.make_batches(inputs, batch_size=10, max_single_len=5,
+
+        batch_size = 10
+        max_single_len = 5
+
+        batches = tools.make_batches(inputs, batch_size, max_single_len,
                 token_item=self.alphabet_size, pad_item=self.alphabet_size+1)
+
+        def make_current_state(state_size):
+            return np.zeros((2, batch_size, state_size))
+
+        curr_states = tuple(map(make_current_state, self.state_sizes))
         for i, batch in enumerate(batches):
-            summary, _ = self.run_batch([self.summary, self.optimize], batch)
+            summary, _, new_states = self.run_batch_with_state([self.summary, self.optimize], batch, curr_states)
             summary_writer.add_summary(summary, i)
             if i % 100 == 0:
-                loss_max, loss_mean, loss_min = self.run_batch([self.loss_max, self.loss_mean, self.loss_min], batch)
+                loss_max, loss_mean, loss_min = self.run_batch([self.loss_max, self.loss_mean, self.loss_min], batch, curr_states)
                 logging.info('Step %s: loss_max: %s loss_mean: %s loss_min: %s' % (i, loss_max, loss_mean, loss_min))
                 #losses, probabilities = self.sess.run([self.losses, self.probabilities], feed_dict={self.inputs: batch})
                 #logging.info('Step %s: losses: %s probs: %s' % (i, losses, probabilities))
+            curr_states = new_states
         logging.info('Saved summaries to %s' % summaries_dir)
 
     def sample(self, starting=[], max_num=1000):
