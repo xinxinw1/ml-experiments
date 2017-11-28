1b03dace6de01456e0a20ec52d48e2c6d57013bd
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2017-11-28 00:22:26 -0500

    working run_on_server

--- train/count-1-0.py
+++ train/count-1-0.py
@@ -7,5 +7,5 @@ import numpy as np
 
 model = lstm.LSTMModel('count-1-0', 2)
 data = [(lambda n: [1]*n + [0]*n)(np.random.randint(0, 10)) for i in range(100000)]
-model.train(data, count=True)
+model.train(data, count=True, autosave=True)
 model.save_to_file()
